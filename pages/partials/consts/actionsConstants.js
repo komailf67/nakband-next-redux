4f3b83696c22bb5343c1911ad99c1ed45870495f
komailf67 <komail.f67@gmail.com>
2020-04-20 01:07:26 +0430

    add product with success alert and reset form inputs

--- pages/partials/consts/actionsConstants.js
+++ pages/partials/consts/actionsConstants.js
@@ -3,3 +3,4 @@ export const PRODUCTS = 'PRODUCTS';
 export const ORDERS   = 'ORDERS';
 export const CATEGORIES   = 'CATEGORIES';
 export const ADD_PRODUCTS   = 'ADD_PRODUCTS';
+export const IS_FORM_SUBMITTED   = 'IS_FORM_SUBMITTED';
